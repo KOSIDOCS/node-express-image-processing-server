4402a574274c5329d76b4a4227f320180a861d4b
Kosidocs <kelvinonaru@gmail.com>
2020-10-09 19:16:30 +0400

    added express and path

--- api/app.js
+++ api/app.js
@@ -3,7 +3,5 @@ const path = require('path');
 
 const app = express();
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
-app.use('/*', (request, response) {
-    response.sendFile(pathToIndex);
-});
+app.use('/*', (request, response) => response.sendFile(pathToIndex));
 module.exports = app;
